60cbd71d7ead3b213946b5388d807b53c87864aa
Pavlo <102181982+Pavlo-Orhunov@users.noreply.github.com>
2024-06-12 21:51:06 +0300

    Add files via upload

---
+++ lesson_21/js/script.js
@@ -0,0 +1,20 @@
+"use strict"
+
+// let userName
+
+// let userAge = `56`
+// console.log(typeof userAge)
+// console.log(userAge)
+// userAge = ++userAge
+// console.log(typeof userAge)
+// console.log(userAge)
+
+// const userName = 20
+// userName = "Іштван"
+
+// let numOne = 20
+// let numTwo = `20`
+// // numTwo = +numTwo
+// let numSumm = numOne + numTwo
+// console.log(numSumm)
+// console.log(typeof numSumm)
